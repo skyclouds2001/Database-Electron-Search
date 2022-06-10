fc05e34a590d4348b6ce420588c95804835564b2
skyclouds2001 <skyclouds2001@163.com>
2022-06-10 14:45:51 +0800

    add sql files

---
+++ sql/init-course.sql
@@ -0,0 +1,49 @@
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('数据结构', 4, 'ES201023', 64, '张三', 3, 'B323');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('数据结构', 4, 'ES201013', 64, '李四', 3, 'B325');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('电路分析基础', 4, 'CS201023', 64, '王五', 3, 'B412');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('电路分析基础', 4, 'CS201012', 64, '陈六', 3, 'B415');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('数字电路与逻辑设计', 3, 'ES202003', 48, '张山', 3, 'A213');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('数字电路与逻辑设计', 3, 'ES202011', 48, '张思', 3, 'A225');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('数字电路与逻辑设计', 3, 'ES202015', 48, '王率', 3, 'A207');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('概率论与数理统计', 2.5, 'MS202015', 40, '游强', 3, 'A512');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('概率论与数理统计', 2.5, 'MS202015', 40, '徐火火', 3, 'A510');
+
+INSERT INTO `csy`.`course`
+	(`cou_name`, `cou_credit`, `cou_ser`, `cou_per`, `cou_tea`, `cou_term`, `cou_room`)
+VALUES
+	('概率论与数理统计', 2.5, 'MS202015', 40, '王率', 3, 'A507');
---
+++ sql/init-student.sql
@@ -0,0 +1,24 @@
+INSERT INTO `csy`.`student`
+	(`stu_id`, `stu_name`, `stu_birth`, `stu_sex`, `stu_nation`, `stu_register`, `stu_faculty`, `stu_major`, `stu_class`, `stu_grade`, `stu_email`, `stu_status`)
+VALUES
+	(20009200001, '张三', '2002-2-23', b'0', '中国', '北京东城', '计算机科学与技术', '计算机科学与技术', 2001023, 2020, '20009200001@stu.xidian.edu.cn', b'0');
+
+INSERT INTO `csy`.`student`
+	(`stu_id`, `stu_name`, `stu_birth`, `stu_sex`, `stu_nation`, `stu_register`, `stu_faculty`, `stu_major`, `stu_class`, `stu_grade`, `stu_email`, `stu_status`)
+VALUES
+	(20009200002, '李四', '2001-11-22', b'0', '中国', '陕西西安', '人工智能', '智能科学与技术', 2012003, 2020, '20009200002@stu.xidian.edu.cn', b'0');
+
+INSERT INTO `csy`.`student`
+	(`stu_id`, `stu_name`, `stu_birth`, `stu_sex`, `stu_nation`, `stu_register`, `stu_faculty`, `stu_major`, `stu_class`, `stu_grade`, `stu_email`, `stu_status`)
+VALUES
+	(20009200003, '王五', '2003-5-3', b'1', '中国', '上海浦东', '计算机科学与技术', '软件工程', 2001026, 2020, '20009200003@stu.xidian.edu.cn', b'0');
+
+INSERT INTO `csy`.`student`
+	(`stu_id`, `stu_name`, `stu_birth`, `stu_sex`, `stu_nation`, `stu_register`, `stu_faculty`, `stu_major`, `stu_class`, `stu_grade`, `stu_email`, `stu_status`)
+VALUES
+	(20009200004, '李华', '2002-12-18', b'0', '中国', '浙江杭州', '网络与信息安全', '信息安全', 2018012, 2020, '20009200004@stu.xidian.edu.cn', b'0');
+
+INSERT INTO `csy`.`student`
+	(`stu_id`, `stu_name`, `stu_birth`, `stu_sex`, `stu_nation`, `stu_register`, `stu_faculty`, `stu_major`, `stu_class`, `stu_grade`, `stu_email`, `stu_status`)
+VALUES
+	(20009200005, '李明', '2001-9-27', b'1', '中国', '陕西延安', '计算机科学与技术', '计算机科学与技术', 2001024, 2020, '20009200005@stu.xidian.edu.cn', b'0');
